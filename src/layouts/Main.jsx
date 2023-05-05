2d2fa246e75542953eed991d50607c2b22b0275e
parvejpappu-100 <parvejhasanpappu85@gmail.com>
2023-05-05 20:23:18 +0600

    added ScrollRestoration

--- src/layouts/Main.jsx
+++ src/layouts/Main.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from '../pages/Shared/Header/Header';
 import { Outlet, useNavigation } from 'react-router-dom';
 import Footer from '../pages/Shared/Footer/Footer';
+import { ScrollRestoration } from "react-router-dom";
 
 const Main = () => {
     const navigation = useNavigation();
@@ -9,6 +10,7 @@ const Main = () => {
         <div>
             <Header></Header>
             <div className='lg:container mx-auto text-2xl text-center my-5'>{navigation.state === "loading" && <progress className="progress w-56"></progress>}</div>
+            <ScrollRestoration></ScrollRestoration>
             <Outlet></Outlet>
             <Footer></Footer>
         </div>
